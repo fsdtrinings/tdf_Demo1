bccb1cc9a2f2755806fa217943c6f3b25708444d
fsdtrainings <javafsd.trainings@gmail.com>
2021-11-02 11:33:21 +0530

    tdf form class work

---
+++ src/app/Account.ts
@@ -0,0 +1,5 @@
+export class Account
+{
+    constructor(public name:string,public balance:number,public email:string)
+    {}
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { Account } from './Account';
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TDF-App';
+
+  accountObj:Account = new Account('',0,'');
+
+
+  onSubmit(accountForm:NgForm)
+  {
+    console.log("form submitted..."+accountForm.value.name);
+
+    this.accountObj.name = accountForm.value.name;
+    this.accountObj.balance = accountForm.value.balance;
+    this.accountObj.email = accountForm.value.email;
+
+    console.log(this.accountObj);
+    
+  }
 }
